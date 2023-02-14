68bc67a2f70c130fe630ea8196bb79666c7e7a80
LouisV <louis.vansenden@cultureamp.com>
2023-02-14 15:37:40 +0800

    Add gallery

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css"
 
-function App() {
+import LightGallery from "lightgallery/react"
+
+// import styles
+import "lightgallery/css/lightgallery.css"
+import "lightgallery/css/lg-zoom.css"
+import "lightgallery/css/lg-thumbnail.css"
+import "lightgallery/css/lg-video.css"
+
+import lgThumbnail from "lightgallery/plugins/thumbnail"
+import lgZoom from "lightgallery/plugins/zoom"
+import lgVideo from "lightgallery/plugins/video"
+
+const videos = ["Birak", "Bunuru", "Djeran", "Djilba", "Kambarang", "Makuru"]
+
+export default function App() {
+  const onInit = () => {
+    console.log("lightGallery has been initialized")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      className="App"
+      style={{
+        marginTop: 100,
+      }}
+    >
+      <div className="galleryContainer">
+        <LightGallery
+          onInit={onInit}
+          speed={500}
+          plugins={[lgThumbnail, lgZoom, lgVideo]}
         >
-          Learn React
-        </a>
-      </header>
+          {videos.map((video) => (
+            <a
+              key={video}
+              data-lg-size="1280-720"
+              data-video={`{"source": [{"src":"/video/${video}.mp4", "type":"video/mp4"}], "attributes": {"preload": false, "playsinline": true, "controls": true}}`}
+              data-poster={`/video/thumb/${video}.jpg`}
+              data-sub-html={`<h4>'${video} Season</h4>`}
+            >
+              <img
+                width="600"
+                height="320"
+                style={{ margin: 10 }}
+                class="img-responsive"
+                src={`/video/thumb/${video}.jpg`}
+                alt={`${video} Season`}
+              />
+            </a>
+          ))}
+        </LightGallery>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
